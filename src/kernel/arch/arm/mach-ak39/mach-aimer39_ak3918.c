68b51338551482d4e607e8f3aab0f0fc7743bdd7
root <you@example.com>
2016-07-09 13:15:53 -0500

    modified:   src/kernel/arch/arm/mach-ak39/mach-aimer39_ak3918.c

--- src/kernel/arch/arm/mach-ak39/mach-aimer39_ak3918.c
+++ src/kernel/arch/arm/mach-ak39/mach-aimer39_ak3918.c
@@ -466,7 +466,7 @@ static struct platform_device soc_camera_interface = {
 static struct ak_led_data leds[] = {
 	{
 	.name		= "g_led",
-	.def_trigger	= "default_on",
+	.def_trigger	= "default-on",
 	.gpio		= {
 		.pin		= AK_GPIO_57,
 		.pulldown	= -1,
