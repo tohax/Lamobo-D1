60513433d2e2724637b7d9e9aa4420274e1ddd3f
root <you@example.com>
2016-07-20 11:29:30 -0500

    modified:   src/ipcamera/rootfs/rootfs.conf 	modified:   src/kernel/arch/arm/configs/aimer39_ak3918_D1_defconfig 	renamed:    src/librootfs/akaimer39lib/etc/init.d/akiserver.bak -> src/librootfs/akaimer39lib/etc/akiserver.ini 	renamed:    src/librootfs/akaimer39lib/etc/init.d/camera.bak -> src/librootfs/akaimer39lib/etc/camera.ini 	deleted:    src/librootfs/akaimer39lib/etc/init.d/boa_service.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/camera.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/change_wifi_mode.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/connect.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/device_save.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/ethernet.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/finish_station.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/is_wifi_mode.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/led_demo.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/mode.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/monitor.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/net_demo.sh 	modified:   src/librootfs/akaimer39lib/etc/init.d/power_off.sh 	modified:   src/librootfs/akaimer39lib/etc/init.d/power_on.sh 	modified:   src/librootfs/akaimer39lib/etc/init.d/rc.local 	deleted:    src/librootfs/akaimer39lib/etc/init.d/restart_IPC.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/select_wifi_mode.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/select_wire_mode.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/service.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/udisk.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/update.sh 	modified:   src/librootfs/akaimer39lib/etc/init.d/wifi 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_connect.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_recover.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_softap.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_start.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_station.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_stop.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wifi_wps.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wirenet.sh 	deleted:    src/librootfs/akaimer39lib/etc/init.d/wpa_supplicant.conf 	deleted:    src/librootfs/akaimer39lib/usr/bin/record_video 	deleted:    src/librootfs/akboalib/bin/boa 	deleted:    src/librootfs/akboalib/etc/boa/boa.conf 	deleted:    src/librootfs/akboalib/etc/mime.types 	deleted:    src/librootfs/akboalib/var/www/cgi-bin/helloworld.cgi 	modified:   src/librootfs/akwifilib/etc/wpa_supplicant.conf

--- src/librootfs/akaimer39lib/etc/init.d/power_off.sh
+++ src/librootfs/akaimer39lib/etc/init.d/power_off.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+2>/dev/null if pgrep wpa_supplicant; then killall -9 wpa_supplicant; fi
+2>/dev/null if pgrep dropbear; then killall -9 dropbear; fi
--- src/librootfs/akaimer39lib/etc/init.d/power_on.sh
+++ src/librootfs/akaimer39lib/etc/init.d/power_on.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-/etc/init.d/wifi
+2>/dev/null if pgrep dropbear; then killall -9 dropbear; fi
+2>/dev/null if pgrep wpa_supplicant; then killall -9 wpa_supplicant; fi
+2>/dev/null wpa_supplicant -B -iwlan0 -Dwext -c /etc/wpa_supplicant.conf
+if  iwlist wlan0 scan | grep -i TP 1>/dev/null ; then
+# здесь проверка уровня сигнала
+        /etc/init.d/wifi
+        mkdir -p /etc/dropbear
+                if [ ! -f /etc/dropbear/dropbear_rsa_host_key ]; then
+                    dropbearkey -t rsa -f /etc/dropbear/dropbear_rsa_host_key
+                    dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+                    dropbearkey -t dss -f /etc/dropbear/dropbear_dss_host_key
+                fi
+        dropbear -B
+        ntpd -q -p time.windows.com
+#здесь запуск rsync
+else
+        2>/dev/null killall -9 wpa_supplicant
+        2>/dev/null killall -9 dropbear
+fi
