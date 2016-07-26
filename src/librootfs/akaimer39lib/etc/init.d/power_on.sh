afe1698aa92fb1c6fe1f875dcdd12e125b4fac94
root <you@example.com>
2016-07-26 13:42:26 -0500

    modified:   src/librootfs/akaimer39lib/etc/init.d/power_on.sh 	modified:   src/librootfs/akaimer39lib/etc/init.d/wifi 	modified:   src/librootfs/akaimer39lib/etc/mdev.conf

--- src/librootfs/akaimer39lib/etc/init.d/power_on.sh
+++ src/librootfs/akaimer39lib/etc/init.d/power_on.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
+AP=bus
+Server=10.10.10.2
+if pgrep wpa_supplicant; then killall -9 wpa_supplicant; fi
+if pgrep dropbear; then killall -9 dropbear; fi
 wpa_supplicant -B -iwlan0 -Dwext -c /etc/wpa_supplicant.conf
-if iwlist wlan0 scan | grep -i TP 1>/dev/null ; then
-	/etc/init.d/wifi
-	Signal=$(iwconfig wlan0 | grep Link | cut -d "=" -f 2 | cut -d "/" -f 1)
-	if [ $Signal -ge 30 ]; then
+if iwlist wlan0 scan | grep -i $AP 1>/dev/null ; then
+        /etc/init.d/wifi
+        Signal=$(iwconfig wlan0 | grep Link | cut -d "=" -f 2 | cut -d "/" -f 1)
+        if [ $Signal -ge 30 ]; then
          dropbear -B
          ntpd -q -p time.windows.com
-	 #здесь запуск rsync
-	echo heartbeat > /sys/class/leds/r_led/trigger
-	sleep 0.5
-	echo heartbeat > /sys/class/leds/g_led/trigger
+         sleep 3
+         #здесь запуск rsync
+        echo heartbeat > /sys/class/leds/r_led/trigger
+        sleep 0.5
+        echo heartbeat > /sys/class/leds/g_led/trigger
 
-dropbearkey -y -f /etc/dropbear/rsa_key | grep ssh | DROPBEAR_PASSWORD='root' dbclient -y root@192.168.1.109 'cat >> .ssh/authorized_keys && echo "Key copied"'
-rsync -avz --log-file=/etc/rsync.txt -e "dbclient -y -i /etc/dropbear/rsa_key" /mnt/`hostname` root@192.168.1.109:/mnt/hdd/oneday
+if [ -d /mnt/`hostname` ]; then rsync -avz --log-file=/etc/rsync.txt -e "ssh -y -i /etc/dropbear/dropbear_rsa_host_key" /mnt/`hostname` root@$Server:/mnt/hdd/oneday; fi
 echo default-on > /sys/class/leds/r_led/trigger
 echo default-on > /sys/class/leds/g_led/trigger
 # конец rsync
-	fi
+        fi
 else
         if pgrep wpa_supplicant; then killall -9 wpa_supplicant; fi
         if pgrep dropbear; then killall -9 dropbear; fi
 fi
+
