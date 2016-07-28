84c9de2de6ac505de8cec54e7e9776848976abb5
root <you@example.com>
2016-07-28 06:58:23 -0500

    modified:   src/librootfs/akaimer39lib/etc/init.d/power_off.sh

--- src/librootfs/akaimer39lib/etc/init.d/power_off.sh
+++ src/librootfs/akaimer39lib/etc/init.d/power_off.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-if [[ "`pgrep camera.sh`" = "" ]]; then /etc/init.d/camera.sh; fi
-echo 1 > /sys/class/leds/g_led/brightness
-/etc/init.d/wifi_led.sh r_led blink 1000 900
 if pgrep wpa_supplicant; then killall -9 wpa_supplicant; fi
 rm -rf /var/run/wpa_supplicant
 if pgrep dropbear; then killall -9 dropbear; fi
+#ifdown wlan0
+if [[ "`pgrep camera.sh`" = "" ]]; then
+/etc/init.d/camera.sh fi
+echo 1 > /sys/class/leds/g_led/brightness
+/etc/init.d/wifi_led.sh r_led blink 1000 900
+fi
