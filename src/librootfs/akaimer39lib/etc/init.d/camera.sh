39cb275564174a1d70e3a6eb26881ca934e39247
tohax <tohax@mail.ru>
2016-09-03 11:11:49 +0500

    Update camera.sh

--- src/librootfs/akaimer39lib/etc/init.d/camera.sh
+++ src/librootfs/akaimer39lib/etc/init.d/camera.sh
@@ -9,5 +9,5 @@ chmod 755 $RECORD_PATH
 fi
 while true
 do
-/etc/init.d/record_video -t 300 -p $RECORD_PATH -P 1 -w 640 -h 480 -r 1 -l 0 -v 0 -q 40 -m 0 -b 100000 -a 2 2>/dev/null
+/etc/init.d/record_video -t 300 -p $RECORD_PATH -P 2 -w 640 -h 480 -r 1 -l 0 -v 0 -q 40 -m 0 -b 100000 -a 2 2>/dev/null
 done
