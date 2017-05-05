8512eb05a645480202f196b15656917528dc6b29
tohax <tohax@mail.ru>
2017-05-05 09:33:33 +0500

    Update power_on.sh

--- src/librootfs/akaimer39lib/etc/init.d/power_on.sh
+++ src/librootfs/akaimer39lib/etc/init.d/power_on.sh
@@ -10,33 +10,35 @@ sleep 3
  export HOME=/etc
  Signal=$(iwconfig wlan0 | grep Signal | cut -d "-" -f 2 | cut -d " " -f 1)
 	if [ $Signal -le 85 ] > /dev/null; then
-		if pidof camera.sh; then kill `pidof camera.sh`; fi
+		if pidof camera.sh; then kill -2 `pidof camera.sh`; fi
                 if pgrep record_video; then kill -2 `pgrep record_video`;fi
-		if pgrep dropbear; then kill `pgrep dropbear`; fi
+		if pgrep dropbear; then kill -2 `pgrep dropbear`; fi
 		dropbear -B
+		ntpd -q -p time.windows.com
+		sleep 10
 		hwclock --systohc
 		#здесь запуск rsync
 		echo heartbeat > /sys/class/leds/r_led/trigger
 		sleep 1
 		echo heartbeat > /sys/class/leds/g_led/trigger
 		echo `hwclock` > /etc/on_`hostname`.txt
-		umount -l /mnt
-		mount /dev/mmcblk0p1 /mnt
+		#umount -l /mnt
+		#mount /dev/mmcblk0p1 /mnt
 		find /mnt/ -name "*index" -exec rm {} \;
 		# copy on.txt
 		time=`date +%Y%m%d`
-		rsync -av --no-o --no-g --remove-source-files --password-file=/etc/.rsync /etc/on_`hostname`.txt root@$Server::video/Avtobus/$time/
+		rsync -av --no-o --no-g --remove-source-files --password-file=/etc/.rsync /etc/on_`hostname`.txt root@$Server::video/ya.disk/Avtobus/$time/
 		#ssh -i /etc/dropbear/dropbear_rsa_host_key root@$Server "bash -c '/root/dropbox_uploader.sh upload /mnt/hdd/Avtobus/$time/on_`hostname`.txt  /Avtobus/$time/`hostname`/on_`hostname`.txt'"
 		# copy main files
 		rsync -av --no-o --no-g --remove-source-files --log-file=/etc/`hostname`.txt --password-file=/etc/.rsync /mnt/ root@$Server::video/oneday/
 		# copy log file
-		rsync -av --no-o --no-g --remove-source-files --password-file=/etc/.rsync /etc/`hostname`.txt root@$Server::video/Avtobus/$time/
+		rsync -av --no-o --no-g --remove-source-files --password-file=/etc/.rsync /etc/`hostname`.txt root@$Server::video/ya.disk/Avtobus/$time/
 		#ssh -i /etc/dropbear/dropbear_rsa_host_key root@$Server "bash -c '/root/dropbox_uploader.sh upload /mnt/hdd/Avtobus/$time/`hostname`.txt  /Avtobus/$time/`hostname`/`hostname`.txt'"
 		find /mnt/[2-9]* -type d -delete
-		if [ -d /mnt/[2-9]* ]; then
-		sync
-		reboot
-		fi
+		#if [ -d /mnt/[2-9]* ]; then
+		#sync
+		#reboot
+		#fi
 		/etc/init.d/wifi_led.sh r_led on
 		/etc/init.d/wifi_led.sh g_led off
 		# конец rsync
