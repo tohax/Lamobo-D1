59d03b207ea9e7b6f109d123b1613e0727378e3d
tohax <tohax@mail.ru>
2017-09-10 17:13:57 +0500

    Update camera.sh

--- src/librootfs/akaimer39lib/etc/init.d/camera.sh
+++ src/librootfs/akaimer39lib/etc/init.d/camera.sh
@@ -9,7 +9,7 @@ while true
  do
 TIME=`date +%H`
 if [ $TIME -ge 23 ] && [ $TIME -le 5 ]; then exit; fi
- /etc/init.d/record_video -t 300 -p $RECORD_PATH -P 2 -w 640 -h 480 -r 1 -l 0 -v 0 -q 40 -m 0 -b 10000 -a 2 2>/dev/null
+ /etc/init.d/record_video -t 300 -p $RECORD_PATH -P 1 -w 640 -h 480 -r 1 -l 0 -v 0 -q 40 -m 0 -b 10000 -a 2 2>/dev/null
  echo 3 > /proc/sys/vm/drop_caches
  sync
 done
